e0eaa94b10f133a217e397857fbdd337ac6cb4dc
Frandoma <franklindoria@yahoo.es>
2023-08-23 16:45:41 -0500

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      const task = {
+        id: tasks.length + 1,
+        content: newTask,
+      };
+
+      setTasks([...tasks, task]);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>Lista de Tareas</h1>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Nueva tarea"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <ul className="task-list">
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.content}
+            <button onClick={() => handleDeleteTask(task.id)}>
+            <span className="material-icons">delete</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
